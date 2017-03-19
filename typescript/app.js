c5d2bb35c2469c12c02bfb4b04825e45bf940b60
Nauzet Melián Batista <nauzet18@gmail.com>
2017-03-19 19:12:17 +0000

    Realizado el examen de la seccion de typescript.

--- typescript/app.js
+++ typescript/app.js
@@ -1,109 +1,32 @@
-var _this = this;
-var WHO_GOD = " dios nauzet ";
-function saludar(nombre, quien, donde) {
-    if (quien === void 0) { quien = "nadie"; }
-    console.log("Hola mi " + (WHO_GOD + nombre.toUpperCase()));
+var nombre = "Ricardo Tapia";
+var edad = 23;
+var PERSONAJE = {
+    nombre: nombre,
+    edad: edad
+};
+var batman = {
+    nombre: "Bruno Díaz",
+    artesMarciales: ["Karate", "Aikido", "Wing Chun", "Jiu-Jitsu"]
+};
+var resultadoDoble = function (a, b) { return (a + b) * 2; };
+function getAvenger(nombre, poder, arma) {
+    if (arma === void 0) { arma = "cuchillo"; }
+    var mensaje;
+    if (poder) {
+        mensaje = nombre + " tiene el poder de: " + poder + " y un arma: " + arma;
+    }
+    else {
+        mensaje = nombre + " tiene un " + poder;
+    }
 }
-var persona = { nombre: "nauzet" };
-var apellidos = "Melián Batista";
-var edad = 123;
-var existe = true;
-var fechaNacimiento = new Date();
-console.log("la fecha de nacimiento fechaNacimiento");
-saludar(persona.nombre, "nauzet", "aqui");
-var miFunction = function (a) {
-    return a;
-};
-var miFunctionF = function (a) { return a; };
-var miFunction2 = function (a, b) {
-    return a + b;
-};
-var miFunctionF2 = function (a, b) { return a + b; };
-var miFunction3 = function (a, b) {
-    var suma = a + b;
-    return suma;
-};
-var miFunctionF3 = function (a, b) {
-    var suma = a + b;
-    return suma;
-};
-var miObjeto = {
-    nombre: "nauzet",
-    saludar: function () {
-        console.log("Hola " + this.nombre);
+;
+var Rectangulo = (function () {
+    function Rectangulo(base, altura) {
+        this.base = base;
+        this.altura = altura;
     }
-};
-miObjeto.saludar();
-var miObjetoF = {
-    nombre: "nauzet",
-    saludar: function () { return console.log("Hola FF " + _this.nombre); }
-};
-miObjetoF.saludar();
-var miObjetoT = {
-    nombre: "nauzet",
-    saludar: function () {
-        setTimeout(function () { console.log("Hola T " + this.nombre); }, 1500);
-    }
-};
-miObjetoT.saludar();
-var miObjetoTF = {
-    nombre: "nauzet",
-    saludar: function () {
-        var _this = this;
-        setTimeout(function () { console.log("Hola TF " + _this.nombre); }, 1500);
-    }
-};
-miObjetoTF.saludar();
-var miObjetoTAmbito = {
-    nombre: "nauzet",
-    saludar: function () {
-        var ambito = this;
-        setTimeout(function () { console.log("Hola TAmbito " + ambito.nombre); }, 1500);
-    }
-};
-miObjetoTAmbito.saludar();
-var averger = {
-    nombre: "Steve",
-    clave: "capitan america",
-    poder: "fuerza"
-};
-var nombre = averger.nombre, clave = averger.clave, poder = averger.poder;
-var avengers = ['thor', 'tony', 'hulk'];
-var dios = avengers[0], ironman = avengers[1], hulk = avengers[2];
-var prom1 = new Promise(function (resolve, reject) {
-    setTimeout(function () {
-        console.log("Promesa terminada");
-        resolve();
-    }, 1500);
-});
-console.log("paso 1");
-prom1.then(function () {
-    console.log("termino bien");
-}, function () {
-    console.error("termino mal");
-});
-console.log("paso 2");
-function enviarMision(xmen) {
-    console.log("Enviando a " + xmen.nombre);
-}
-var personajeX = { nombre: "Picara", poder: "camuflaje" };
-enviarMision(personajeX);
-function enviarMisionI(xmen) {
-    console.log("Enviando a " + xmen.nombre);
-}
-var personajeXI = { nombre: "Picara", poder: "camuflaje" };
-enviarMisionI(personajeXI);
-var Persona = (function () {
-    function Persona(nombre, apellidos) {
-        this.genero = "masculino";
-        this.nombre = nombre;
-        this.apellidos = apellidos;
-    }
-    Persona.prototype.getFullName = function () {
-        return this.nombre + " " + this.apellidos;
+    Rectangulo.prototype.area = function () {
+        return this.base * this.altura;
     };
-    return Persona;
+    return Rectangulo;
 }());
-var yo = new Persona("Nauzet", "Melian Batista");
-console.log(yo);
-console.log(yo.getFullName());
--- typescript/app.ts
+++ typescript/app.ts
@@ -1,3 +1,4 @@
+/*
 const WHO_GOD = " dios nauzet ";
 
 function saludar (nombre:string, quien:string = "nadie", donde?:string )
@@ -206,3 +207,80 @@ class Persona {
 let yo:Persona = new Persona( "Nauzet", "Melian Batista");
 console.log(yo);
 console.log(yo.getFullName());
+*/
+
+//------------ Examen
+
+// Uso de Let y Const
+let nombre:string = "Ricardo Tapia";
+let edad:number = 23;
+
+const PERSONAJE:{nombre:string, edad: number} = {
+  nombre: nombre,
+  edad: edad
+};
+
+
+// Cree una interfaz que sirva para validar el siguiente objeto
+interface heroe{
+  nombre:string,
+  artesMarciales:string[]
+}
+var batman:heroe = {
+  nombre: "Bruno Díaz",
+  artesMarciales: ["Karate","Aikido","Wing Chun","Jiu-Jitsu"]
+}
+
+// Convertir esta funcion a una funcion de flecha
+/*
+function resultadoDoble( a, b ){
+  return (a + b) * 2
+}
+*/
+let resultadoDoble = ( a:number, b:number ) =>  (a + b) * 2;
+
+// Función con parametros obligatorios, opcionales y por defecto
+// donde NOMBRE = obligatorio
+//       PODER  = opcional
+//       ARMA   = por defecto = "arco"
+/*
+function getAvenger( nombre, poder, arma ){
+  var mensaje;
+  if( poder ){
+     mensaje = nombre + " tiene el poder de: " + poder + " y un arma: " + arma;
+  }else{
+     mensaje = nombre + " tiene un " + poder
+  }
+};
+*/
+function getAvenger( nombre:string, poder?:string, arma:string = "cuchillo" ){
+  var mensaje;
+  if( poder ){
+    mensaje = `${nombre} tiene el poder de: ${poder} y un arma: ${arma}`;
+  }else{
+    mensaje = `${nombre} tiene un ${poder}`;
+  }
+};
+
+// Cree una clase que permita manejar la siguiente estructura
+// La clase se debe de llamar rectangulo,
+// debe de tener dos propiedades:
+//   * base
+//   * altura
+// También un método que calcule el área  =  base * altura,
+// ese método debe de retornar un numero.
+class Rectangulo {
+  base: number;
+  altura: number;
+
+  constructor (base:number, altura:number)
+  {
+    this.base = base;
+    this.altura = altura;
+  }
+
+  public area ():number
+  {
+    return this.base * this.altura;
+  }
+}
