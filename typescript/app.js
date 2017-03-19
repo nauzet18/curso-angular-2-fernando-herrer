aa453d1d3482fa690791966ef7e8aa4361d679b2
Nauzet Melián Batista <nauzet18@gmail.com>
2017-03-19 11:48:05 +0000

    Probando las funciones de flecha.

--- typescript/app.js
+++ typescript/app.js
@@ -10,3 +10,19 @@ var existe = true;
 var fechaNacimiento = new Date();
 console.log("la fecha de nacimiento fechaNacimiento");
 saludar(persona.nombre, "nauzet", "aqui");
+var miFunction = function (a) {
+    return a;
+};
+var miFunctionF = function (a) { return a; };
+var miFunction2 = function (a, b) {
+    return a + b;
+};
+var miFunctionF2 = function (a, b) { return a + b; };
+var miFunction3 = function (a, b) {
+    var suma = a + b;
+    return suma;
+};
+var miFunctionF3 = function (a, b) {
+    var suma = a + b;
+    return suma;
+};
--- typescript/app.ts
+++ typescript/app.ts
@@ -15,3 +15,35 @@ let fechaNacimiento:Date = new Date();
 console.log(`la fecha de nacimiento fechaNacimiento` );
 
 saludar(persona.nombre, "nauzet", "aqui");
+
+
+//-------funciones de flecha
+
+//Declaro una funcion anonima normal y la asigno a una variable.
+let miFunction =  function (a)
+{
+  return a;
+}
+
+//Declaro una funcion anonima con la sintaxis de flecha y la asigno a una variable.
+let miFunctionF = (a) => a;
+
+//Lo mismo que hantes pero con mas parametros y fijando los tipos
+let miFunction2 =  function (a:number, b:number)
+{
+  return a + b;
+}
+
+let miFunctionF2 = (a:number, b:number) => a +b;
+
+//Lo mismo, pero con funciones con un cuerpo de varias lineas
+let miFunction3 =  function (a:number, b:number)
+{
+  let suma = a + b;
+  return suma;
+}
+
+let miFunctionF3 = (a:number, b:number) => {
+  let suma = a + b;
+  return suma;
+};
