10812e90bb0a6f3dfa1611af156cd4450ef16f4c
Nauzet Melián Batista <nauzet18@gmail.com>
2017-03-19 11:33:06 +0000

    probando las string plantillas. Probar como se sustituyen valores puesto en los string

--- typescript/app.js
+++ typescript/app.js
@@ -1,13 +1,12 @@
+var WHO_GOD = " dios nauzet ";
 function saludar(nombre, quien, donde) {
     if (quien === void 0) { quien = "nadie"; }
-    console.log("Hola mi " + WHO_GOD + nombre.toUpperCase());
+    console.log("Hola mi " + (WHO_GOD + nombre.toUpperCase()));
 }
-var WHO_GOD = " dios nauzet ";
 var persona = { nombre: "nauzet" };
 var apellidos = "Melián Batista";
 var edad = 123;
 var existe = true;
 var fechaNacimiento = new Date();
-"";
-console.log("la fecha de nacimiento " + fechaNacimiento);
+console.log("la fecha de nacimiento fechaNacimiento");
 saludar(persona.nombre, "nauzet", "aqui");
--- typescript/app.ts
+++ typescript/app.ts
@@ -1,9 +1,9 @@
+const WHO_GOD = " dios nauzet ";
+
 function saludar (nombre:string, quien:string = "nadie", donde?:string )
 {
-  console.log("Hola mi "+WHO_GOD+nombre.toUpperCase() );
+  console.log( `Hola mi ${WHO_GOD+nombre.toUpperCase() }` );
 }
-
-const WHO_GOD = " dios nauzet ";
 
 let persona = { nombre: "nauzet" };
 let apellidos:string = "Melián Batista";
@@ -11,7 +11,7 @@ let edad:number = 123;
 let existe:boolean = true;
 let fechaNacimiento:Date = new Date();
 
-``
-console.log("la fecha de nacimiento "+fechaNacimiento);
+
+console.log(`la fecha de nacimiento fechaNacimiento` );
 
 saludar(persona.nombre, "nauzet", "aqui");
