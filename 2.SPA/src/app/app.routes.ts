9c29b8ffd0963663e3c72d6cc00f533626d4d81c
Nauzet Melián Batista <nauzet18@gmail.com>
2017-03-21 19:29:21 +0000

    Se ha creado las rutas para about y heroes, se han instanciados y se ha creado el menu en navbar para ponerlos links. Se ha puesto lo de link activo. Se ha añadido una animacion fadein

--- 2.SPA/src/app/app.routes.ts
+++ 2.SPA/src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
 
 const APP_ROUTES: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'heroes', component: HeroesComponent },
   { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
