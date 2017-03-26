99ab82dba4a8c1d66b446f16cda228006e87997b
Nauzet Melián Batista <nauzet18@gmail.com>
2017-03-26 13:09:30 +0100

    Implementando ejemplo de todos los pipes. Aun no estan todos

--- 3.pipes/src/app/app.component.ts
+++ 3.pipes/src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  nombre = 'Nauzet';
+  array:number[] = [1,2,3,4,5,6,7,8,9,10];
+  PI = Math.PI;
+  a:number = 0.234;
+  salario:number = 1234.5;
+
+  objheroe= {
+    nombre: 'nauzet',
+    edad: 20,
+    clave: 'dios',
+    direccion: {
+      calle: 'en todos lados',
+      casa: 'sin casa'
+    }
+  };
+
+  valorPromesa = new Promise( ( resolve, reject )=> {
+    setTimeout( ()=> resolve('llego la data!') ,3500 );
+  }
+  );
+
+  fecha = new Date();
 }
