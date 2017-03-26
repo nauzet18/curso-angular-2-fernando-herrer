114e7bcbc69081e2ab1d3384126f7aa186879ed0
Nauzet Melián Batista <nauzet18@gmail.com>
2017-03-26 13:12:59 +0100

    Añadida la configuración del angular core, parar establecer la configuración del idioma a español. Esto es para que en el ejemplo del pipe Date, las fechas se generen en español

--- 3.pipes/src/app/app.module.ts
+++ 3.pipes/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
@@ -14,7 +14,9 @@ import { AppComponent } from './app.component';
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'es' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
