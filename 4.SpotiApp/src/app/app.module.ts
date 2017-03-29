bbe0c6fdbfdca78e7c71c42f80a5ae4698c6a578
Nauzet Melián Batista <nauzet18@gmail.com>
2017-03-29 20:42:57 +0100

    Se han creado los componentes para la navegación, el home y el search. Se ha creado las rutas y se ha maquetado en el home un carrusel. Se han puesto las rutas y enlaces a las vistas.

--- 4.SpotiApp/src/app/app.module.ts
+++ 4.SpotiApp/src/app/app.module.ts
@@ -3,16 +3,26 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+//Routes
+import {APP_ROUTING} from './app.routes';
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
+import { NavbarComponent } from './components/share/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    APP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ 4.SpotiApp/src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
+
+const APP_ROUTES: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'search', component: SearchComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }
+];
+
+export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES, {useHash:true});
+//NOTA: Si para la redirección queremos usar la # ya que es mas compatible con navegadores.
+//pasamos el parametro useHash en la llamada de forRoot
+//export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES, { useHash : true});
