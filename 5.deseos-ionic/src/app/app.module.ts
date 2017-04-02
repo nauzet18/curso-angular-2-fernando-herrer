13a5110020f7bb6af75fb4bcc746afbe8cfac950
Nauzet Melián Batista <nauzet18@gmail.com>
2017-04-02 13:37:37 +0100

    Se han eliminado las paginas contact y home.About la dejo de ejemplo por que usa navcontroler y quiero saber q es. Creo dos paginas nuevas y las cargo donde toca

--- 5.deseos-ionic/src/app/app.module.ts
+++ 5.deseos-ionic/src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+
+import { PendientesComponent } from '../pages/pendientes/pendientes.component';
+import { TerminadosComponent } from '../pages/terminados/terminados.component';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -12,10 +12,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
+    TabsPage,
+    PendientesComponent,
+    TerminadosComponent
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -23,10 +22,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
+    TabsPage,
+    PendientesComponent,
+    TerminadosComponent
   ],
   providers: [
     StatusBar,
--- 5.deseos-ionic/src/pages/tabs/tabs.ts
+++ 5.deseos-ionic/src/pages/tabs/tabs.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
-
-import { HomePage } from '../home/home';
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
+import { PendientesComponent } from '../pendientes/pendientes.component';
+import { TerminadosComponent } from '../terminados/terminados.component';
 
 @Component({
   templateUrl: 'tabs.html'
@@ -10,9 +8,8 @@ import { ContactPage } from '../contact/contact';
 export class TabsPage {
   // this tells the tabs component which Pages
   // should be each tab's root Page
-  tab1Root: any = HomePage;
-  tab2Root: any = AboutPage;
-  tab3Root: any = ContactPage;
+  tab1Root: any = PendientesComponent;
+  tab2Root: any = TerminadosComponent;
 
   constructor() {
 
